6b059d50c04d8c5ce34e8ff8adb3561f54719d9a
David Romero <d.romero@sapalomera.cat>
2025-03-24 15:46:10 +0100

    created needed controllers

---
+++ app/Http/Controllers/controller-login.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controller-login extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/controller-search_bar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controller-search_bar extends Controller
+{
+    //
+}
